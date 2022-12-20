37ba0414371849cf6962d1d9ecdb6fdbab083437
EC2 Default User <ec2-user@ip-172-31-83-241.ec2.internal>
2022-12-20 21:33:53 +0000

    Figuring out graphql

--- agile-app/src/components/TheWelcome.vue
+++ agile-app/src/components/TheWelcome.vue
@@ -1,34 +1,43 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
+    <h1>Blog App</h1>
     <input type="text" v-model="name" placeholder="Blog name" />
-    <input type="text" v-model="posts.item.title" placeholder="Blog Post" />
-    <button v-on:click="createBlog">Create Todo</button>
+    <input type="text" v-model="posts" placeholder="Post" />
+    <button v-on:click="createBlog">Create Blog</button>
     <div v-for="item in blogs" :key="item.id">
       <h3>{{ item.name }}</h3>
-      <p>{{ item.post.title }}</p>
+    </div>
+    <input type="text" v-model="title" placeholder="Post Title" />
+    <input type="text" v-model="comments" placeholder="Post Comments"/> 
+    <div v-for="item in posts" :key="item.id">
+      <h2>{{ item.title }}</h2>
+      <h2>{{ item.comments }}</h2>
     </div>
   </div>
 </template>
 
 <script>
   import { API } from 'aws-amplify';
-  import { createBlog } from '../graphql/mutations';
-  import { listBlogs } from '../graphql/queries';
+  import { createBlog, createPost } from '../graphql/mutations';
+  import { listBlogs, listPosts } from '../graphql/queries';
 
   export default {
     name: 'app',
     async created() {
       this.getBlogs();
+      this.getPosts();
     },
     data() {
       return {
         name: '',
-        posts: {}, 
+        posts: [],
         blogs: []
       };
     },
     methods: {
+      async createPost(){
+        
+      },
       async createBlog() {
         const { name, posts } = this;
         if (!name || !posts) return;
@@ -38,14 +47,19 @@
           variables: { input: blog }
         });
         this.name = '';
-        this.posts = {};
       },
       async getBlogs() {
         const blogs = await API.graphql({
           query: listBlogs
         });
         this.blogs = blogs.data.listBlogs.items;
-        console.log(blogs.data)
+        console.log(blogs.data.listBlogs.items)
+      }, 
+      async getPosts() {
+        const posts = await API.graphql({
+          query: listPosts
+        });
+        this.posts = posts.data.listPosts.items;
       }
     }
   };
